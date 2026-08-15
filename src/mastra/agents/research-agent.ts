c1ea9a49573cbc60283ce618c1684cbc73fdc8d7
LPT4 <example@lpt4.dev>
2026-08-14 23:35:05 -0300

    new web API ahh

--- src/mastra/agents/research-agent.ts
+++ src/mastra/agents/research-agent.ts
@@ -1,7 +1,7 @@
 import { Agent } from "@mastra/core/agent";
 import { getVehicle } from "../tools/get-vehicle";
 import { createVehicle } from "../tools/create-vehicle";
-import { webFetch } from "../tools/web-access";
+import { webFetch, webSearch } from "../tools/web-access";
 
 import { Memory } from "@mastra/memory";
 
@@ -25,10 +25,10 @@ export const researchAgent = new Agent({
     If the information is already in the database return the information to the user and do not
     use any other tools.
 
-    If the information is not already in the database use the webFetch tool to make
+    If the information is not already in the database use the webSearch and webFetch tools to make
     a web search to get the average price and common issues of the make, model and year.
 
-    After using the webFetch tool use the createVehicle tool to add the vehicle information
+    After using the webSearch and webFetch tools use the createVehicle tool to add the vehicle information
     to the database. Seperate the make, model and year and put them in the respected fields move all
     other information to the description field. Finally give the result from the webResearch tool to the user.
 
@@ -40,6 +40,6 @@ export const researchAgent = new Agent({
   model: "google/gemini-3.5-flash-lite",
 
   tools: {
-    createVehicle, getVehicle, webFetch
+    createVehicle, getVehicle, webFetch, webSearch
   },
 });
